f3a3c668148b4cea08a6f536a51dedccbb422e13
seoungcoding <tjdxo11933@naver.com>
2020-11-27 16:58:24 +0900

    프로그래머스 문제

--- src/ProgrammersProblem/Price.java
+++ src/ProgrammersProblem/Price.java
@@ -21,6 +21,28 @@ package ProgrammersProblem;
  * 5초 시점의 ₩3은 0초간 가격이 떨어지지 않았습니다.
  */
 public class Price {
+    public static void main(String[] args) {
+        int prices[] = {1, 2, 3, 2, 3};
+        solution(prices);
 
+    }
+    public static void solution(int[] prices) {
+        int[] answer= new int[prices.length];
+        int cnt;
+        for(int i = 0; i < prices.length; i++){
+            cnt = 0;
+            for (int j = i+1 ; j <prices.length; j++){
+                if (prices[i] <= prices[j]){
+                    cnt++;
+                }else {
+                    cnt++;
+                    break;
+                }
+            }
+            answer[i] = cnt;
+        }
+        for (int i= 0; i < answer.length; i++)
+            System.out.print(answer[i]+" ");
+    }
 
 }
